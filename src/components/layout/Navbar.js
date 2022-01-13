b1e08adf9a12cd1ece9b37ab3a56a75ea2c5ff05
Corneliu Laur <cl179971@gmail.com>
2022-01-13 10:05:23 +0000

    initial commit

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+
+const Navbar = ({ icon, title }) => {
+    
+        return (
+            <nav className='navbar bg-primary'>
+                <h1>
+                    <i className={icon}/>{ title }
+                </h1>
+            </nav>
+        )
+}
+
+// optional: in case we don't put any props, this will by default display
+Navbar.defaultProps = {
+    title: ' Github Finder',
+    icon: 'fab fa-github'
+}
+
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+}
+
+export default Navbar
