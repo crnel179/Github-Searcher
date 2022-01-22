914a5f758deef618cc1fbb790f4b99e0bb6e32b3
Corneliu Laur <cl179971@gmail.com>
2022-01-22 05:38:03 +0000

    added a spinner, search input and submit btn

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import axios from 'axios';
 import './App.css';
 
@@ -11,9 +12,10 @@ class App extends Component {
   }
   
   async componentDidMount() {
+    console.log(process.env.REACT_APP_GITHUB_CLIENT_ID);
     this.setState({ loading: true });
 
-    const res = await axios.get('https://api.github.com/users');
+    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
     this.setState({ users: res.data, loading: false });
   }
@@ -23,6 +25,7 @@ class App extends Component {
       <div className="App">        
         <Navbar title=' Github Searcher ' icon='fab fa-github'/>
         <div className='container'>
+          <Search />
           <Users loading={this.state.loading} users={this.state.users}/>
         </div>
       </div>
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,14 +1,24 @@
-import React, { Component } from 'react'
+import React from 'react'
 import UserItem from './UserItem'
+import Spinner from '../layout/Spinner'
+import PropTypes from 'prop-types';
 
-class Users extends Component {
-    render() {
+
+const Users = ({ users, loading }) => {
+    if(loading) {
+        return <Spinner />
+    } else {
         return (
             <div style={userStyle}>
-                {this.props.users.map(user => <UserItem key={user.id} user={user}/>)}
+                {users.map(user => <UserItem key={user.id} user={user}/>)}
             </div>
         )
     }
+}
+
+Users.protoTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.boolean.isRequired
 }
 
 const userStyle = {
